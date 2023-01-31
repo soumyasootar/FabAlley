889907d20b871684bf6da7f9ebc3b40aaee266ce
Pkanand1999 <singhisking810267@gmail.com>
2023-01-31 18:48:59 +0530

    productOnly for testing

---
+++ ProductPage/sc.js
@@ -0,0 +1,269 @@
+
+let Product = [{
+  "id": "1",
+  "category": "tops",
+  "title": "Pink White Floral Ruffle Sleeve Peplum Top",
+  "discount": "56",
+  "price2": "1450",
+  "price1": "638",
+  "site": "https://www.faballey.com/pink-white-floral-ruffle-sleeve-peplum-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05210Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05210Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05210Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05210Z/d8.jpg"
+},
+{
+  "id": "2",
+  "category": "tops",
+  "title": "Pink Floral Smocked Peplum Top",
+  "discount": "56",
+  "price2": "1650",
+  "price1": "726",
+  "site": "https://www.faballey.com/floral-pink-smoking-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP04999Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP04999Z/d5.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP04999Z/d8.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP04999Z/d4.jpg"
+},
+{
+  "id": "3",
+  "category": "tops",
+  "title": "Black Organza Ruffled  Shoulder Crop Top",
+  "discount": "56",
+  "price2": "1600",
+  "price1": "704",
+  "site": "https://www.faballey.com/black-organza-ruffled--shoulder-crop-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP06263Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP06263Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP06263Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP06263Z/d8.jpg"
+},
+{
+  "id": "4",
+  "category": "tops",
+  "title": "Powder Blue Lace Trim Detail Top",
+  "discount": "56",
+  "price2": "1600",
+  "price1": "704",
+  "site": "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg"
+},
+{
+  "id": "5",
+  "category": "tops",
+  "title": "Grey Velvet Lace Trim Buttoned Blouse",
+  "discount": "56",
+  "price2": "1900",
+  "price1": "836",
+  "site": "https://www.faballey.com/grey-velvet-lace-trim-buttoned-blouse-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP06308Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP06308Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP06308Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP06308Z/d8.jpg"
+},
+{
+  "id": "6",
+  "category": "tops",
+  "title": "White Abstract Wrap Peplum Top",
+  "discount": "56",
+  "price2": "1600",
+  "price1": "704",
+  "site": "https://www.faballey.com/white-abstract-overlap-front-smocked-peplum-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05877Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05877Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05877Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05877Z/d8.jpg"
+},
+{
+  "id": "7",
+  "category": "tops",
+  "title": "Pink Bardot Trim Smocked Crop Top",
+  "discount": "56",
+  "price2": "1500",
+  "price1": "660",
+  "site": "https://www.faballey.com/pink-bardot-trim-smocked-crop-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05977Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05977Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05977Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05977Z/d8.jpg"
+},
+{
+  "id": "8",
+  "category": "tops",
+  "title": "Pink Floral Frilled Bardot Top",
+  "discount": "40",
+  "price2": "1600",
+  "price1": "960",
+  "site": "https://www.faballey.com/pink-floral-frilled-bardot-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP06162Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP06162Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP06162Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP06162Z/d8.jpg"
+},
+{
+  "id": "9",
+  "category": "tops",
+  "title": "Dusty Pink Front Ruched Peplum Top",
+  "discount": "56",
+  "price2": "1400",
+  "price1": "616",
+  "site": "https://www.faballey.com/dusty-pink-front-ruched-peplum-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05272Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05272Z/d5.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05272Z/d4.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05272Z/d8.jpg"
+},
+{
+  "id": "10",
+  "category": "tops",
+  "title": "Pink Solid Puff Sleeve Wrap Top",
+  "discount": "56",
+  "price2": "1500",
+  "price1": "660",
+  "site": "https://www.faballey.com/pink-solid-puff-sleeve-wrap-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP04893Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP04893Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP04893Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP04893Z/d8.jpg"
+},
+{
+  "id": "11",
+  "category": "tops",
+  "title": "Multicolored Floral Front Tie Up Blouse",
+  "discount": "56",
+  "price2": "1500",
+  "price1": "660",
+  "site": "https://www.faballey.com/multicolored-floral-neck-tie-up-blouse-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05374Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05374Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05374Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05374Z/d8.jpg"
+},
+{
+  "id": "12",
+  "category": "tops",
+  "title": "Lilac Front Tie Buttoned Smocked Sleeve Shirt",
+  "discount": "56",
+  "price2": "1400",
+  "price1": "616",
+  "site": "https://www.faballey.com/lilac-front-tie-buttoned-smocked-sleeve-shirt-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05934Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05934Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05934Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05934Z/d8.jpg"
+},
+{
+  "id": "13",
+  "category": "tops",
+  "title": "Rust Polka Collared Front Knot Crop Shirt",
+  "discount": "56",
+  "price2": "1400",
+  "price1": "616",
+  "site": "https://www.faballey.com/rust-polka-collared-front-knot-crop-shirt-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05275Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05275Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05275Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05275Z/d8.jpg"
+},
+{
+  "id": "14",
+  "category": "tops",
+  "title": "Pink Floral Wrap Top",
+  "discount": "61",
+  "price2": "1500",
+  "price1": "585",
+  "site": "https://www.faballey.com/pink-white-printed-wrap-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05050Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05050Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05050Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05050Z/d8.jpg"
+},
+{
+  "id": "15",
+  "category": "tops",
+  "title": "Hot Pink Puff Sleeve Smocked Top",
+  "discount": "40",
+  "price2": "1400",
+  "price1": "840",
+  "site": "https://www.faballey.com/hot-pink-puff-sleeve-smocked-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP06346Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP06346Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP06346Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP06346Z/d8.jpg"
+},
+{
+  "id": "16",
+  "category": "tops",
+  "title": "Dusty Pink Gathered Peplum Top",
+  "discount": "56",
+  "price2": "1400",
+  "price1": "616",
+  "site": "https://www.faballey.com/dusty-pink-gathered-peplum-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05204Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05204Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05204Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05204Z/d8.jpg"
+},
+{
+  "id": "17",
+  "category": "tops",
+  "title": "Wine Embellished Halter Asymmetric Top",
+  "discount": "56",
+  "price2": "1500",
+  "price1": "660",
+  "site": "https://www.faballey.com/wine-embellished-halter-asymmetric-top-78/prdt",
+  "img1": "https://img.faballey.com/images/Product/TOP05476Z/1.jpg",
+  "img2": "https://img.faballey.com/images/Product/TOP05476Z/d4.jpg",
+  "img3": "https://img.faballey.com/images/Product/TOP05476Z/d5.jpg",
+  "img4": "https://img.faballey.com/images/Product/TOP05476Z/d8.jpg"
+}];
+
+localStorage.setItem("FabAlleyProduct", JSON.stringify(Product));
+let data = JSON.parse(localStorage.getItem("FabAlleyProduct"));
+display(data)
+function display(data) {
+  document.querySelector("#product_tops").innerHTML = ""
+  data.map(function (ele) {
+    // if(ele.category=="dress"){
+
+    document.querySelector("#product_tops").innerHTML +=
+      `
+            <div id="product_single_item_container">
+            <div id="image_hover">
+                <img src="${ele.img2}" alt="" id="product_single_item_container_image">
+                <span id="product_offer">${ele.discount}%<br>OFF</span>
+                
+              </div>
+                <div id="product_name_div"><p>${ele.title}</p></div>
+                <div id="product_price_div">
+                <span id="selling_price">₹${ele.price1}</span>
+                <h3 id="printed_price">₹${ele.price2}</h3>
+                </div>
+                <div id="product_express_div">
+                <i class="fa-solid fa-truck-fast" id="express_icon"></i>
+                <h4>EXPRESS</h4>
+                <h5>3 Day Delivery</h5>
+                </div>
+                <div id="product_cartWishlist_div">
+                <i class="fa-regular fa-heart"></i>
+                <button id="add_to_bag">ADD TO BAG</button>
+                </div>
+                
+
+            </div>
+            `
+    // }
+  })
+}
+
+async function fetching() {
+  let res = await fetch("http://localhost:3000/products")
+  let json = await res.json();
+  display(json)
+  console.log(res)
+  console.log(json)
+}
+fetching();

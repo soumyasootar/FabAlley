1b32dd88022348b3206f944400f1a5c9820a5c89
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-02-03 05:12:49 +0530

    BAG IN CART DONE

--- Carts/components/cart.js
+++ Carts/components/cart.js
@@ -49,7 +49,72 @@ let CARTSarray = [
     img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
     img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
     img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
-  }, 
+  },
+  {
+    id: "4",
+    category: "tops",
+    title: "Powder Blue Lace Trim Detail Top",
+    discount: "56",
+    price2: "1600",
+    price1: "704",
+    site: "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+    img1: "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+    img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+    img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+    img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
+  },
+  {
+    id: "4",
+    category: "tops",
+    title: "Powder Blue Lace Trim Detail Top",
+    discount: "56",
+    price2: "1600",
+    price1: "704",
+    site: "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+    img1: "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+    img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+    img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+    img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
+  },
+  {
+    id: "4",
+    category: "tops",
+    title: "Powder Blue Lace Trim Detail Top",
+    discount: "56",
+    price2: "1600",
+    price1: "704",
+    site: "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+    img1: "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+    img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+    img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+    img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
+  },
+  {
+    id: "4",
+    category: "tops",
+    title: "Powder Blue Lace Trim Detail Top",
+    discount: "56",
+    price2: "1600",
+    price1: "704",
+    site: "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+    img1: "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+    img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+    img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+    img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
+  },
+  {
+    id: "4",
+    category: "tops",
+    title: "Powder Blue Lace Trim Detail Top",
+    discount: "56",
+    price2: "1600",
+    price1: "704",
+    site: "https://www.faballey.com/powder-blue-lace-trim-detail-top-78/prdt",
+    img1: "https://img.faballey.com/images/Product/TOP05720Z/1.jpg",
+    img2: "https://img.faballey.com/images/Product/TOP05720Z/d4.jpg",
+    img3: "https://img.faballey.com/images/Product/TOP05720Z/d5.jpg",
+    img4: "https://img.faballey.com/images/Product/TOP05720Z/d8.jpg",
+  },
 ];
 localStorage.setItem("mycart", JSON.stringify(CARTSarray));
 
@@ -62,122 +127,154 @@ Procuctsssarray.forEach(function (elem) {
 localStorage.setItem("mycart", JSON.stringify(Procuctsssarray));
 
 displayCart(Procuctsssarray);
-
 function displayCart(temp) {
-  document.getElementById("productBox").textContent="";
-  temp.map(function (elem,index) {
-   var maindiv = document.createElement("div");
-    maindiv.id = "recbox";
+  let product=document.getElementById("productBox")
+  product.innerHTML=""
+  temp.map(function(elem,i){
+    let box=document.createElement("div")
+    box.id="box-box"
+    let box1=document.createElement("div")
+    box1.id="box1"
+    let box11=document.createElement("div")
+    box11.id="box11"
+    let box1a=document.createElement("div")
+    box1a.id="box1a"
+    let boxprice=document.createElement("div")
+    boxprice.id="boxprice"
+    let boxbottom=document.createElement("div")
+    boxbottom.id="boxbottom"
 
-    let imgDiv = document.createElement("div");
-    imgDiv.id = "forimage";
+    let image=document.createElement("img")
+    image.src=elem.img1
+    image.className="boximage"
 
-    let image = document.createElement("img");
-    image.src = elem.img1;
+    let name=document.createElement("p")
+    name.id="boxname"
+    name.innerText=elem.title
 
-    imgDiv.append(image);
+    let size=document.createElement("p")
+    size.id="box-grey"
+    size.innerText="Size: M";
+    let SKU = document.createElement("p");
+    SKU.id="box-grey"
+    SKU.textContent = "SKU CODE : SWT00154C";
 
-    let descriptionDiv = document.createElement("div");
-    descriptionDiv.id = "dressnames";
+    let quantity= document.createElement("select")
+    quantity.id= "qty"
+    quantity.style.cursor="pointer"
+    let option = document.createElement("option");
+    option.value = "";
+    option.text = "QTY";
+    option.innerText="QTY"
+    quantity.append(option)
 
-    let dressName = document.createElement("h3");
-    dressName.textContent = elem.title;
+    for (let i = 1; i <= 10; i++) {
+      let option = document.createElement("option");
+      option.value = i;
+      option.text = i;
+      quantity.append(option);
+    }
+    let quantitytxt= document.createElement("p")
+    quantitytxt.className= "qtytxt"
+    quantitytxt.id= "box-grey"
+    quantitytxt.textContent="QTY : "+elem.quantity
 
-    let size = document.createElement("p");
-    size.textContent = "Size: M";
+    box1a.append(name,size,SKU,quantity,quantitytxt)
+    
+    let price1 = document.createElement("p")
+    price1.id="boxprice1"
+    price1.textContent = "₹"+elem.price1;
 
-    let price1 = document.createElement("p");
-    price1.textContent = elem.price1;
 
     let price2 = document.createElement("p");
     price2.style.textDecoration = "line-through";
-    price2.textContent = elem.price2;
+    price2.id="boxprice2"
+    price2.textContent ="₹"+ elem.price2;
 
-    let Quantity = document.createElement("select");
+    boxprice.append(price1,price2)
 
-    Quantity.id = "qty";
+    let wishlist = document.createElement("button");
+    wishlist.innerText = "MOVE TO WISHLIST";
+    wishlist.style.cursor="pointer"
+    wishlist.addEventListener("click", function () {
+      wishlistadd(elem,i);
+    });
 
-    let opt0 = document.createElement("option");
-    opt0.setAttribute('value','')
-    opt0.textContent = "select quantity";
-    let opt1 = document.createElement("option");
-    opt1.setAttribute('value','1')
-    opt1.textContent = "1";
+    let REMOVE = document.createElement("button");
 
-
-    let opt2 = document.createElement("option");
-    opt2.setAttribute('value','2')
-    opt2.textContent = "2";
-
-    let opt3 = document.createElement("option");
-    opt3.setAttribute('value','3')
-    opt3.textContent = "3";
-
-    let opt4 = document.createElement("option");
-    opt4.setAttribute('value','4')
-    opt4.textContent = "4";
-
-    let opt5 = document.createElement("option");
-    opt5.setAttribute('value',"5")
-    opt5.textContent = "5";
-
-    let opt6 = document.createElement("option");
-    opt6.setAttribute('value',"6")
-    opt6.textContent = "6";
-
-    let opt7 = document.createElement("option");
-    opt7.setAttribute('value',"7")
-    opt7.textContent = "7";
-
-    let removebtn= document.createElement("button")
-    removebtn.textContent="Remove";
-    removebtn.addEventListener('click',function(){
-      removeItem(elem,index)
-    })
-
-
-    Quantity.append(opt0,opt1, opt2, opt3, opt3, opt4, opt5, opt6, opt7);
-    
-    descriptionDiv.append(dressName, size, Quantity, price1, price2,removebtn);
-    
-    Quantity.addEventListener('change',function(){
-      quantity(elem,index,maindiv)
-    })
-    maindiv.append(imgDiv, descriptionDiv);
-    document.getElementById("productBox").append(maindiv);
-  });
+    REMOVE.innerText = "REMOVE";
+    REMOVE.addEventListener("click", function () {
+            removeItem(elem, i);
+    });
+    quantity.addEventListener("change", function () {
+            quantityitems(elem, i, box);
+    });
+    REMOVE.style.cursor="pointer"
+    boxbottom.append(wishlist,REMOVE)
+    box11.append(box1a,boxprice)
+    box1.append(box11,boxbottom)
+    box.append(image,box1)
+    product.append(box)
+  })
   totalprice();
-
-
+  document.getElementById("cartcount").innerText = temp.length;
 }
 
 //reduce function
+function totalprice() {
+  let subtotal = Procuctsssarray.reduce(function (elem, curr) {
+    return elem + Number(curr.price1 * curr.quantity);
+  }, 0);
+  document.getElementById("subtotalll1").innerText = `${subtotal}`;
+  document.getElementById("subtotalll2").innerText = `${subtotal}`;
 
-
-  function totalprice (){
-    let subtotal = Procuctsssarray.reduce(function(elem,curr){
-        return elem + Number(curr.price1*curr.quantity);
-    },0)
-    document.getElementById("subtotalll1").innerText = `${subtotal}`;
-    document.getElementById("subtotalll2").innerText = `${subtotal}`;
-
-    localStorage.setItem("local-subtotal",subtotal)
+  localStorage.setItem("local-subtotal", subtotal);
 }
 
-function quantity(elem,index,div){
+function quantityitems(elem, index, div) {
   let quantity = div.querySelector("#qty").value;
   elem.quantity = Number(quantity);
-  console.log(elem)
+  console.log(elem);
   localStorage.setItem("mycart", JSON.stringify(Procuctsssarray));
-  
-  displayCart(Procuctsssarray);
-}
- 
-function removeItem(elem,index){
-  event.preventDefault();
-  Procuctsssarray.splice(index,1);   
-  localStorage.setItem("mycart", JSON.stringify(Procuctsssarray));
-  displayCart(Procuctsssarray);
- 
 
+  displayCart(Procuctsssarray);
 }
+
+function removeItem(elem, index) {
+  event.preventDefault();
+  Procuctsssarray.splice(index, 1);
+  localStorage.setItem("mycart", JSON.stringify(Procuctsssarray));
+  displayCart(Procuctsssarray);
+}
+
+function wishlistadd(elem,index){
+  let wishcart=JSON.parse(localStorage.getItem("mywishlistcart"))||[]
+  wishcart.push(elem)
+  Procuctsssarray.splice(index, 1);
+  localStorage.setItem("mywishlistcart",JSON.stringify(wishcart))
+  displayCart(Procuctsssarray);
+}
+
+//coupon
+document.getElementById("CouponBtn").addEventListener("click",()=>{
+  let x= document.getElementById("CouPonID").value
+  if(x=="FAB1000"){
+    document.getElementById("discount11").innerText="1000"
+    let tprice=JSON.parse(localStorage.getItem("local-subtotal"))||0
+    tprice-=1000
+    localStorage.setItem("local-subtotal", tprice);
+    document.getElementById("CouponApplied").innerText="Coupon Applied"
+    document.getElementById("CouponApplied").style.color="green"
+    document.getElementById("subtotalll2").innerText = tprice
+  }
+  else{
+    document.getElementById("CouponApplied").innerText="Wrong Coupon Applied"
+    document.getElementById("CouponApplied").style.color="red"
+  }
+})
+
+//username 
+var naaaaame = "YOOOOO"
+localStorage.setItem("usernamefab",JSON.stringify(naaaaame))
+var user1name=JSON.parse(localStorage.getItem("usernamefab"))||"User"
+document.getElementById("custname").innerText=user1name

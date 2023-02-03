68bd8c3529c51614c732a2cc2884724f28540446
Pkanand1999 <singhisking810267@gmail.com>
2023-02-04 00:44:39 +0530

    all page done linking left

--- fabAlleyProfile/UserProfile/mywhishlist/mywishlist.js
+++ fabAlleyProfile/UserProfile/mywhishlist/mywishlist.js
@@ -1,33 +1,33 @@
-// let Product = [{
-//   id: "1",
-//   category: "tops",
-//   title: "Pink White Floral Ruffle Sleeve Peplum Top",
-//   discount: "56",
-//   price2: "1450",
-//   price1: "638",
-//   colour:"pink",
-//   site: "https://www.faballey.com/pink-white-floral-ruffle-sleeve-peplum-top-78/prdt",
-//   img1: "https://img.faballey.com/images/Product/TOP05210Z/1.jpg",
-//   img2: "https://img.faballey.com/images/Product/TOP05210Z/d4.jpg",
-//   img3: "https://img.faballey.com/images/Product/TOP05210Z/d5.jpg",
-//   img4: "https://img.faballey.com/images/Product/TOP05210Z/d8.jpg",
-// },
-// {
-//   id: "2",
-//   category: "tops",
-//   title: "Pink Floral Smocked Peplum Top",
-//   discount: "56",
-//   price2: "1650",
-//   price1: "726",
-//   colour:"pink",
-//   site: "https://www.faballey.com/floral-pink-smoking-top-78/prdt",
-//   img1: "https://img.faballey.com/images/Product/TOP04999Z/1.jpg",
-//   img2: "https://img.faballey.com/images/Product/TOP04999Z/d5.jpg",
-//   img3: "https://img.faballey.com/images/Product/TOP04999Z/d8.jpg",
-//   img4: "https://img.faballey.com/images/Product/TOP04999Z/d4.jpg",
-// },]
+let Product = [{
+  id: "1",
+  category: "tops",
+  title: "Pink White Floral Ruffle Sleeve Peplum Top",
+  discount: "56",
+  price2: "1450",
+  price1: "638",
+  colour:"pink",
+  site: "https://www.faballey.com/pink-white-floral-ruffle-sleeve-peplum-top-78/prdt",
+  img1: "https://img.faballey.com/images/Product/TOP05210Z/1.jpg",
+  img2: "https://img.faballey.com/images/Product/TOP05210Z/d4.jpg",
+  img3: "https://img.faballey.com/images/Product/TOP05210Z/d5.jpg",
+  img4: "https://img.faballey.com/images/Product/TOP05210Z/d8.jpg",
+},
+{
+  id: "2",
+  category: "tops",
+  title: "Pink Floral Smocked Peplum Top",
+  discount: "56",
+  price2: "1650",
+  price1: "726",
+  colour:"pink",
+  site: "https://www.faballey.com/floral-pink-smoking-top-78/prdt",
+  img1: "https://img.faballey.com/images/Product/TOP04999Z/1.jpg",
+  img2: "https://img.faballey.com/images/Product/TOP04999Z/d5.jpg",
+  img3: "https://img.faballey.com/images/Product/TOP04999Z/d8.jpg",
+  img4: "https://img.faballey.com/images/Product/TOP04999Z/d4.jpg",
+},]
 
-let Product= JSON.parse(localStorage.getItem("mywishlistcart")) || [];
+// let Product= JSON.parse(localStorage.getItem("mywishlistcart")) || [];
 
 display(Product)
 function display(data){
@@ -53,7 +53,7 @@ function display(data){
               <button class="add_to_bag${ind}">ADD TO BAG</button>
             </div>
             <div id="crs_btn">
-              <button>X</button>
+              <button onclick="remove(${ind})">X</button>
             </div>
           </div>
         </div>
@@ -82,4 +82,8 @@ function cart(e) {
         localStorage.setItem("mycart",JSON.stringify(cart_product))
   }
   console.log(cart_product)
+}
+function remove(ind){
+  Product.splice(ind,1)
+  display(Product)
 }
